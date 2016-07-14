9c716d09ae0f954ed53a93acff7ed3575efc623f
ben wu <benwu9999@gmail.com>
2016-07-13 21:20:09 -0400

    c

---
+++ src/main/java/com/wungong/commonmodelservice/model/Skill.java
@@ -0,0 +1,36 @@
+package com.wungong.commonmodelservice.model;
+
+import java.util.UUID;
+
+import com.datastax.driver.mapping.annotations.PartitionKey;
+import com.datastax.driver.mapping.annotations.Table;
+
+@Table(name = "skills")
+public class Skill {
+	
+	@PartitionKey
+	private UUID skillId;
+	
+	private String description;
+	
+	public Skill(UUID skillId) {
+		this.skillId = skillId;
+	}
+
+	public UUID getSkillId() {
+		return skillId;
+	}
+
+	public void setSkillId(UUID skillId) {
+		this.skillId = skillId;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
